681be0a3ad27157eaae79d10b6caf8a9b49478f5
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2023-07-24 13:20:22 +0530

    form handling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,36 @@
-import React,{useState} from 'react';
-interface ButtonProps{
-    text:string | number | boolean,
-    onClick?:()=>void
+import React, { useState } from 'react';
+interface ButtonProps {
+    text: string | number | boolean,
+    onClick?: () => void
 }
 
 
+const Button: React.FC<ButtonProps> = (props) => {
+    const { text,onClick } = props;
+    const [value, setValue] = useState<string | undefined>();
 
-//for giving object 
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
-interface Book{
-    name:string,
-    price:number
-}
+        setValue(e.target.value)
+    }
 
-const Button:React.FC<ButtonProps>=(props)=>{
-    const {text}=props;
-    const [value,setValue]=useState<Book>({
-        name:'one',
-        price:10
-    });
-    return(
-        <div>
-        <h3>{value.name}(RS.{value.price})</h3>
-        <button onClick={()=>setValue({name:'TWO',price:20})}>
-{text}
-        </button>
+    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        console.log(e);
+        
+
+    }
+    
+
+      
+     
+            return (
+                <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleNameChange} placeholder='Enter your Name' />
+            <h1>{value}</h1>
+            <button type='submit' >Submit</button>
+            </form>
         </div>
     )
 }
